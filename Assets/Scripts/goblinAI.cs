2bc0a1f2662f8a72237f8a9558097e8c35dd627a
MoonMan <ainurfatkullin9@gmail.com>
2025-10-19 23:14:59 +0900

    add goblins push

--- Assets/Scripts/goblinAI.cs
+++ Assets/Scripts/goblinAI.cs
@@ -1,15 +1,26 @@
+// using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class goblinAI : MonoBehaviour
 {
+    [SerializeField] private float separationRadius = 1.0f;
+    [SerializeField] private float pushForce = 0.5f;
     [SerializeField] private int damage = 10;
     private NavMeshAgent _agent;
+    private Rigidbody _rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _rb = GetComponent<Rigidbody>();
+
+        _rb.isKinematic = true;
     }
 
     // Update is called once per frame
@@ -23,6 +34,22 @@ public class goblinAI : MonoBehaviour
         {
             Debug.LogWarning("Player not found for goblin: " + gameObject.name);
             _agent.isStopped = true;
+        }
+
+        ApplySeparation();
+    }
+
+    void ApplySeparation()
+    {
+        Collider[] nearby = Physics.OverlapSphere(transform.position, separationRadius);
+
+        foreach (var col in nearby)
+        {
+            if (col.CompareTag("Goblin") && col.transform != transform)
+            {
+                Vector3 direction = (transform.position - col.transform.position).normalized;
+                transform.position += direction * pushForce * Time.deltaTime;
+            }
         }
     }
 
